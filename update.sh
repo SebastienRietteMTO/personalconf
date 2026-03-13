4aa3e3b384a62f62c97ff65b0b693c252e4afed6
Sébastien Riette <sebastien.riette@meteo.fr>
2026-03-13 09:45:59 +0100

    Check if a sub package has been updated

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -x
+
 # Script to update repository and install
 # The script can be run more frequently than the desired frequency
 # We check the time elapsed since to determine whether to perform
@@ -10,6 +12,22 @@
 
 function usage {
   echo "Usage: $0 [-h] [--daily] [--hourly]"
+}
+
+function check_update {
+  # Try to update (pull) and return true if a new version has been checked out
+  commit=$(git rev-parse HEAD)
+  output="$output $(git pull 2>&1)"
+  if [ $? -ne 0 ]; then
+    echo "${output}"
+    exit 2
+  fi
+  if [ $(git rev-parse HEAD) != $commit ]; then
+    result=true
+  else
+    result=false
+  fi
+  echo $result
 }
 
 delay=0
@@ -30,14 +48,16 @@ fi
 if [ $(($(date +%s) - $(date -r ~/.personalconf_last_update +%s))) -ge $delay ]; then
   touch ~/.personalconf_last_update
   cd "$(dirname "${BASH_SOURCE[0]}")"
-  commit=$(git rev-parse HEAD)
-  output=$(git pull 2>&1)
-  if [ $? -ne 0 ]; then
-    echo "${output}"
-    exit 2
-  fi
-  if [ $(git rev-parse HEAD) != $commit ]; then
-    # Only perform installation if the repository has been updated
+  output=""
+  do_install=$(check_update)
+  cd sub
+  for sub in *; do
+    result=$(check_update)
+    if [ $result = true ]; then
+      do_install=true
+    fi
+  done
+  if [ $do_install = true ]; then
     output="${output} $(./install.sh)"
     if [ $? -ne 0 ]; then
       echo "${output}"
